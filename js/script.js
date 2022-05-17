fa2c9d40c75d05560ad60e8f2081041d59a9400e
HenningJ99 <90407614+HenningJ99@users.noreply.github.com>
2022-05-17 16:11:17 +0200

    Improved JS for different window sizes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function resetImg(img) {
+  img.setAttribute('style', '')
   img.setAttribute('class', 'img-fluid card-top-img')
   img.setAttribute('onclick', 'enlargeImg(this)')
 
@@ -6,15 +7,28 @@ function resetImg(img) {
 // Function to increase image size
 function enlargeImg(img) {
 
-    var currWidth = img.clientWidth;
-    var currHeight = img.clientHeight;
-    var desired_scale_x = 0.9 * screen.width / currWidth
-    var desired_scale_y = 0.9 * screen.height / currHeight
+    var currWidth = window.innerWidth;
+    var currHeight = window.innerHeight;
+    var desired_scale_x = 0.9 * currWidth / img.width
+    var desired_scale_y = 0.9 * currHeight / img.height
 
-    var image_x = 0.1 * screen.width
-    var image_y = 0.1 * screen.height
+
     img.className = "fullscreen";
     img.setAttribute('onclick', 'resetImg(this)')
+    var elements = document.querySelectorAll('.fullscreen');
+    for(var i=0; i<elements.length; i++){
+        if (desired_scale_x > desired_scale_y) {
+        elements[i].style.height = currHeight + "px";
+        elements[i].style.width = "auto";
+        elements[i].style.left = (currWidth - elements[i].width) / 2 + "px";
+        }
+        else {
+        elements[i].style.width = currWidth + "px";
+        elements[i].style.height = "auto";
+        elements[i].style.top = (currHeight - elements[i].height) / 2 + "px";
+        }
+        elements[i].style.margin = "0 auto";
+}
 
 
 }
